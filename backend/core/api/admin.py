09b8db87fc64a09d328416bf2ca70e428c94b49d
fathani1207 <youssef.fathani1207@gmail.com>
2026-01-08 16:56:41 +0100

    registrer dans admin django

--- backend/core/api/admin.py
+++ backend/core/api/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Carrier
 
 admin.site.register(Product)
+
+@admin.register(Carrier)
+class CarrierAdmin(admin.ModelAdmin):
+    list_display = ('name', 'delay_days')
