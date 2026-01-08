edf5b9035635c50f43b514ab07207e30fa839333
unknown <kaouthar.asrar@gmail.com>
2026-01-08 17:11:06 +0100

    Register Review model in Django admin

--- backend/core/api/admin.py
+++ backend/core/api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from .models import Review
 
-from .models import Product
-
-admin.site.register(Product)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'product', 'rating', 'created_at')
+    list_filter = ('rating', 'product')
