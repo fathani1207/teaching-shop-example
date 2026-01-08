0ee359b6f6b52d3f37653e94cf9687f976cd4fca
Salaheddin BEN HAMMICH <salaheddin.bnh@gmail.com>
2026-01-08 17:03:25 +0100

    feat: register SiteSettings in Django admin

--- backend/core/api/admin.py
+++ backend/core/api/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, SiteSettings
 
 admin.site.register(Product)
+@admin.register(SiteSettings)
+class SiteSettingsAdmin(admin.ModelAdmin):
+    list_display = ('orders_enabled',)
